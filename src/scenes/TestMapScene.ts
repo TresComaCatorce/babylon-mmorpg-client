2581d757f929c35e595ecd7bbbdadb59e9c3cae7
Cristian Ferrero <cristian.ferrero1992@gmail.com>
2025-05-16 01:59:27 -0300

    Work on "TestMapScene" class.

--- src/scenes/TestMapScene.ts
+++ src/scenes/TestMapScene.ts
@@ -1,16 +1,12 @@
 import {
-	PointerEventTypes,
 	ArcRotateCamera,
 	Vector3,
 	HemisphericLight,
 	MeshBuilder,
-	PointerInfo,
 } from '@babylonjs/core';
 
 import GameController from '@mmorpg/controllers/GameController';
 import SCENE_NAMES from '@mmorpg/utils/constants/SCENE_NAMES';
-import ScenesController from '@mmorpg/controllers/ScenesController';
-import LoadingScene from './LoadingScene';
 import MapScene from './MapScene';
 
 class TestMapScene extends MapScene {
@@ -36,29 +32,15 @@ class TestMapScene extends MapScene {
 		// Add light
 		new HemisphericLight('light', new Vector3(0, 1, 0), this);
 
-		// Create sphere
-		const sphere = MeshBuilder.CreateSphere(
-			'sphere',
-			{ diameter: 2 },
-			this,
-		);
-		sphere.position.y = 1;
-
 		// Create ground
-		MeshBuilder.CreateGround('ground', { width: 6, height: 6 }, this);
+		MeshBuilder.CreateGround('ground', { width: 600, height: 600 }, this);
 
-		this.onPointerObservable.add((pointerInfo: PointerInfo) => {
-			switch (pointerInfo.type) {
-				case PointerEventTypes.POINTERDOWN:
-					if (pointerInfo?.pickInfo?.hit) {
-						if (pointerInfo.pickInfo.pickedMesh === sphere) {
-							ScenesController.getInstance().switchToScene(
-								new LoadingScene(),
-							);
-						}
-					}
-					break;
-			}
+		// Create character
+		this.loadAssets('assets/models/warrior.glb').then(() => {
+			console.log('Anims groups...');
+			this.animationGroups.forEach((group) => {
+				console.log(`Anim Group: ${group.name}`);
+			});
 		});
 	}
 }
