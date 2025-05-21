7e0bf5834b5d69052c57eaf17e0a62b6a59a9820
Cristian Ferrero <cristian.ferrero1992@gmail.com>
2025-05-21 15:40:57 -0300

    Add 'g' key to prevent default.

--- src/utils/constants/KEYS_TO_PREVENT_DEFAULT.ts
+++ src/utils/constants/KEYS_TO_PREVENT_DEFAULT.ts
@@ -1,3 +1,3 @@
-const KEYS_TO_PREVENT_DEFAULT = ['w', 'a', 's', 'd', 'arrowup', 'arrowdown', 'arrowleft', 'arrowright', ' '];
+const KEYS_TO_PREVENT_DEFAULT = ['w', 'a', 's', 'd', 'arrowup', 'arrowdown', 'arrowleft', 'arrowright', ' ', 'g'];
 
 export default KEYS_TO_PREVENT_DEFAULT;
