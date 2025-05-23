0f7f0fc4f3c8b8102b8ed883b88a9d0225fcf9c0
Cristian Ferrero <cristian.ferrero1992@gmail.com>
2025-05-23 00:42:48 -0300

    Improve controls info helper.

--- src/ui/helpers/controls-info-helper.ts
+++ src/ui/helpers/controls-info-helper.ts
@@ -5,25 +5,33 @@ const createControlsInfoHelper = (scene: Scene) => {
 	const guiTexture = AdvancedDynamicTexture.CreateFullscreenUI('UI', true, scene);
 
 	const infoBox = new Rectangle();
-	infoBox.width = '60%';
-	infoBox.height = '100px';
+	infoBox.height = '28%';
+	infoBox.width = '28%';
 	infoBox.cornerRadius = 10;
 	infoBox.color = 'white';
 	infoBox.thickness = 1;
 	infoBox.background = '#000000AA';
-	infoBox.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
-	infoBox.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
-	infoBox.paddingBottom = '20px';
+	infoBox.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
+	infoBox.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+	infoBox.paddingTopInPixels = 5;
+	infoBox.paddingLeftInPixels = 5;
 
 	const stackPanel = new StackPanel();
 	stackPanel.isVertical = false;
 	stackPanel.width = '100%';
 	stackPanel.height = '100%';
 
+	const infoTitle = new TextBlock();
+	infoTitle.text = 'Controls';
+	infoTitle.fontSize = 18;
+	infoTitle.textHorizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
+	infoTitle.textVerticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
+	infoTitle.paddingTopInPixels = 8;
+
 	const infoText = new TextBlock();
-	infoText.text = 'Controls\nMovement: WASD || Rotate camera: Hold mouse left-click || Zoom: Mouse scroll';
+	infoText.text = '\n• Movement: W-A-S-D\n• Rotate camera: Hold mouse left-click\n• Zoom: Mouse scroll\n• Run: Shift\n• Run lock/unlock: Caps Lock';
 	infoText.color = 'white';
-	infoText.fontSize = 18;
+	infoText.fontSize = 13;
 	infoText.textHorizontalAlignment = Control.HORIZONTAL_ALIGNMENT_LEFT;
 	infoText.textVerticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
 	infoText.paddingLeft = '10px';
@@ -34,6 +42,7 @@ const createControlsInfoHelper = (scene: Scene) => {
 	closeButton.height = '30px';
 	closeButton.color = 'white';
 	closeButton.background = '#AA0000';
+	closeButton.cornerRadius = 5;
 	closeButton.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_RIGHT;
 	closeButton.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
 	closeButton.paddingTop = '5px';
@@ -57,6 +66,7 @@ const createControlsInfoHelper = (scene: Scene) => {
 		document.body.style.cursor = 'default';
 	});
 
+	infoBox.addControl(infoTitle);
 	stackPanel.addControl(infoText);
 	infoBox.addControl(stackPanel);
 	infoBox.addControl(closeButton);
