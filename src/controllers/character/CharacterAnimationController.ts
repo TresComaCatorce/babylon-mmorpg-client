18c30b578edb19cd2b9f2188c2c2e76413dbacff
Cristian Ferrero <cristian.ferrero1992@gmail.com>
2025-05-27 20:48:42 -0300

    Create empty "CharacterAnimationController".

---
+++ src/controllers/character/CharacterAnimationController.ts
@@ -0,0 +1,5 @@
+import BaseCharacterController from '@mmorpg/controllers/character/BaseCharacterController';
+
+abstract class CharacterAnimationController extends BaseCharacterController {}
+
+export default CharacterAnimationController;
