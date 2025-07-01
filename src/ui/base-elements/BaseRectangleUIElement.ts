8c720dee04a171d38c01137d427cd8d5e5a64986
Cristian Ferrero <cristian.ferrero1992@gmail.com>
2025-07-01 02:15:43 -0300

    Add custom event 'onResizeObservable' to the 'BaseRectangleUIElement' class.

--- src/ui/base-elements/BaseRectangleUIElement.ts
+++ src/ui/base-elements/BaseRectangleUIElement.ts
@@ -1,14 +1,33 @@
-import { Rectangle } from '@babylonjs/gui';
+import { Observable, ICanvasRenderingContext } from '@babylonjs/core';
+import { Measure, Rectangle } from '@babylonjs/gui';
 
 import { IBaseRectangleUIElementConstructorParams } from '@mmorpg/interfaces/ui/base-elements/IBaseRectangleUIElement';
 import IBaseControlUIElement from '@mmorpg/interfaces/ui/base-elements/IBaseControlUIElement';
 
 abstract class BaseRectangleUIElement extends Rectangle implements IBaseControlUIElement {
+	public onResizeObservable = new Observable<{ width: number; height: number }>();
 	private _elementName: string;
+	private _prevWidth = 0;
+	private _prevHeight = 0;
 
 	constructor(params: IBaseRectangleUIElementConstructorParams) {
 		super(params.elementName);
 		this._elementName = params.elementName;
+	}
+
+	protected _processMeasures(parentMeasure: Measure, context: ICanvasRenderingContext) {
+		super._processMeasures(parentMeasure, context);
+
+		const current = this._currentMeasure;
+		const newWidth = current.width;
+		const newHeight = current.height;
+
+		if (newWidth !== this._prevWidth || newHeight !== this._prevHeight) {
+			this._prevWidth = newWidth;
+			this._prevHeight = newHeight;
+
+			this.onResizeObservable.notifyObservers({ width: newWidth, height: newHeight });
+		}
 	}
 
 	get elementName(): string {
